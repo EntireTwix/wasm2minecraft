4ded087d52ae8d48309ab6279f04f2abf977004f
William Katz <willkatz.dev@gmail.com>
2021-03-17 17:22:37 -0700

    :tada:

---
+++ examples/Lock/lock.hpp
@@ -0,0 +1,25 @@
+struct Lock
+{
+private:
+    uint16_t pin;
+    bool state;
+
+public:
+    Lock() = delete;
+    Lock(uint16_t pin) : pin(pin) {}
+
+    bool Get() const
+    {
+        return state;
+    }
+    bool Set(uint16_t attempt, bool new_state)
+    {
+        if (pin != attempt)
+        {
+            return false;
+        }
+
+        state = new_state;
+        return true;
+    }
+};
