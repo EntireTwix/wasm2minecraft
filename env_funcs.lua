d2f13bc1d6025237d5e882ee7fbfd21772e894ac
William Katz <qualitycontentquestionmark@gmail.com>
2021-06-30 13:37:58 -0700

    :art: formatting

--- env_funcs.lua
+++ env_funcs.lua
@@ -1,10 +1,10 @@
 local env = {
---delete operator (C++)
-_ZdlPv = function(reg) reg = nil end,
-
---new operator (C++)
-_Znwm = function(size) return 0 end,
-_Znwj = function(size) return 0 end
-}
-
-return { env = env } 
+    --delete operator (C++)
+    _ZdlPv = function(reg) reg = nil end,
+    
+    --new operator (C++)
+    _Znwm = function(size) return 0 end,
+    _Znwj = function(size) return 0 end
+    }
+    
+    return { env = env } 
