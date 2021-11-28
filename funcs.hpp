8aac4aff4cb644e716d5742e275a19eb1c38d20e
EntireTwix <willkatz.dev@gmail.com>
2021-11-27 21:33:22 -0800

    :sparkles: `lua_memset()` & `lua_memcpy()`

--- funcs.hpp
+++ funcs.hpp
@@ -4,4 +4,22 @@
 #define INLINE __attribute__((always_inline)) inline
 #define any void *
 
+template <typename T>
+void lua_memset(T *ptr, int val, size_t sz)
+{
+    for (size_t i = 0; i < sz; ++i)
+    {
+        ptr[i] = val;
+    }
+}
+
+template <typename T, typename T2>
+void lua_memcpy(T *to, T2 *from, size_t sz)
+{
+    for (size_t i = 0; i < sz; ++i)
+    {
+        from[i] = to[i];
+    }
+}
+
 // put code here
