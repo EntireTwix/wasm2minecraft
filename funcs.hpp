83b50a44d60b4a8b8487c2b1431ddd9a3fdcef4f
EntireTwix <willkatz.dev@gmail.com>
2021-11-29 16:01:59 -0800

    :sparkles: `lua_strlen()`

--- funcs.hpp
+++ funcs.hpp
@@ -22,4 +22,15 @@ void lua_memcpy(T *to, T2 *from, size_t sz)
     }
 }
 
+extern "C"
+{
+    size_t lua_strlen(const char *start)
+    {
+        const char *end = start;
+        for (; *end != '\0'; ++end)
+            ;
+        return end - start;
+    }
+}
+
 // put code here
