2f03801b5cab0428f887101bf54e2383d5e99ca5
William Katz <qualitycontentquestionmark@gmail.com>
2022-01-02 12:02:47 -0800

    :sparkles: added wasm implemented bindings

--- funcs.hpp
+++ funcs.hpp
@@ -10,6 +10,9 @@ extern "C"
     // extern put environmentally defined function here
     extern void wasm_set(char* ptr, char value, size_t pos);
     extern char wasm_get(const char* ptr, size_t pos);
+    extern void* memset(void *ptr, int value, size_t num);
+    extern void* memcpy(void *to, const void *from, size_t sz);
+    extern size_t strlen(const char *start);
 }
 
 // put code here or in the above extern if you want C function symbols
